ba3e84a55fe8cba48cc2144befc07b9fbb0b080b
Erza <eb57603@ubt-uni.net>
2022-12-24 22:57:52 +0100

    regex for email

--- script.js
+++ script.js
@@ -31,13 +31,22 @@ window.onload = () => {
   }
 };
 
+// ky funksioni kthen nje boolean nese email i pranuar si parameter ben match regex per email
+const validateEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
 function validateForm() {
   let email = document.getElementById("email-input").value;
   let password = document.getElementById("password-input").value;
 
-  if (email === "" || password === "") {
-    window.alert("Vendosi te dhenat");
+  if (!validateEmail(email) || password === "" || password.length < 8) {
+    window.alert("Te dhenat e vendosura gabim!");
   } else {
-    window.alert("te dhenat e vendosura");
+    window.alert("Te dhenat jane ne rregull!");
   }
 }
